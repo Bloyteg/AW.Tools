502563b6bc522980224b680bf07e296a87b97175
Joshua Rodgers <bytemr@gmail.com>
2013-07-13 11:08:01 -0600

    Several refactorings and changes.

---
+++ MrByte.Utility/WeakReferenceExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MrByte.Utility
+{
+    public static class WeakReferenceExtension
+    {
+        public static T GetTarget<T>(this WeakReference<T> reference)
+            where T : class
+        {
+            T target;
+
+            if (reference.TryGetTarget(out target))
+            {
+                return target;
+            }
+
+            return null;
+        }
+    }
+}
